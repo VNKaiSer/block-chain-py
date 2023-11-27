e8b2026495761ee6dffbb609f4c59edc7c7da7d5
VNKaiser <tandatvok16@gmail.com>
2023-11-27 12:00:54 +0700

    init blockchain methoad

---
+++ lab03.py
@@ -0,0 +1,24 @@
+class Block: 
+    def __init__():
+        pass
+    def calculate_hash():
+        pass
+
+class BlockChain:
+    def __init__():
+        pass
+    def contruct_genesis():
+        pass
+    
+    @staticmethod
+    def check_validity():
+        pass
+    def new_data(self, sender, recipient, amount):
+        pass
+    @staticmethod
+    def contruct_proof_of_work():
+        pass
+    @property
+    def last_block(self):
+        return self.chain[-1]
+    
